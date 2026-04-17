c3d72664c56a89d17c9444df6d4e9e1e1118b5b4
Carlos Canto <carlosarthur1313@gmail.com>
2026-04-16 23:58:35 -0300

    Implement QR code generation in Estudos_06.R
    Add simple QR code generation example in R.

--- Estudos_06.R
+++ Estudos_06.R
@@ -1,1 +1,12 @@
-
+#Bem simples para fazer o qr code no R
+  #pacote do qr code
+install.packages("qrcode")
+  library(qrcode)
+  #basta colar o link, em que ele gera lá o df
+  code <-  qr_code("link de interesse")
+  #e o o plot mostra o qr code de forma automática já
+  plot(code)
+  
+  #tem a possibilidade de com a função utilizar a generate pra onde voce quer armazenar esse info
+  
+  generate_svg(code,filename = "pasta de destino/qr.svg")
